613e4a859ba9d43dd981e8765953e6f1af767486
Ramiro Martinez <ramiro.nicolas.martinez14@gmail.com>
2024-09-12 11:50:53 -0300

    Agrego funcionalidad de cumplido

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
-const botonAgregar = document.getElementById('btnAgregar');
+const btnAgregar = document.getElementById('btnAgregar');
 const tarea = document.getElementById('toDo');
 const lista = document.getElementById('lista');
-let contador = 0;
-let checkboxex = [];
+const editar = document.getElementById('editar');
+const eliminar = document.getElementById('eliminar');
+const alertaAgregada = document.getElementById('customAlert');
+const btnAlertaAdd = document.getElementById('closeAlert');
+const alertaError = document.getElementById('errorAlert');
+const btnAlertaError = document.getElementById('cerrarAlerta');
 
 function agregarTarea(){
     const nuevoDiv = document.createElement('div');
-    const nuevoInput = document.createElement('input');
-    const textInput = document.createElement('p');
-    nuevoDiv.id=contador;
-    nuevoInput.type = 'checkbox';
-    nuevoInput.id=contador++;
-    textInput.style.display = 'inline';
+    const checkbox = document.createElement('input');
+    const textInput = document.createElement('label');
+    const textInput2 = document.createElement('del');
+    // nuevoDiv.id=contador;
+    // checkbox.id=contador++;
+    checkbox.type = 'checkbox';
     textInput.textContent = tarea.value;
+    textInput2.textContent = tarea.value;
+    textInput2.style.display = 'none';
+
     //ver cómo evalúo cada uno de los checkbox 
     lista.appendChild(nuevoDiv);
-    nuevoDiv.appendChild(nuevoInput);
+    nuevoDiv.appendChild(checkbox);
     nuevoDiv.appendChild(textInput);
+    nuevoDiv.appendChild(textInput2);
     prepararNuevaTarea();
+
+    checkbox.addEventListener('change', (evento) =>{
+        if(evento.target.checked){
+            textInput.style.display = 'none';
+            textInput2.style.display = 'inline';
+        }
+        else{
+            textInput2.style.display = 'none';
+            textInput.style.display = 'inline';
+        }
+    });
+
 }
 
 function prepararNuevaTarea (){
     tarea.value = "";
-
 }
 
 
-botonAgregar.addEventListener('click', ()=>{
-    agregarTarea();    
-    document.getElementById('customAlert').style.display = 'flex';
+btnAgregar.addEventListener('click', ()=>{
+    if(tarea.value != ''){
+        agregarTarea();    
+        alertaAgregada.style.display = 'flex';
+    }
+    else{
+        alertaError.style.display = 'flex';
+    }
 });
 
-document.getElementById('closeAlert').addEventListener('click', () => {
-    document.getElementById('customAlert').style.display = 'none';
+btnAlertaAdd.addEventListener('click', () => {
+    alertaAgregada.style.display = 'none';
 });
 
+
+btnAlertaError.addEventListener('click', () => {
+    alertaError.style.display = 'none';
+});
